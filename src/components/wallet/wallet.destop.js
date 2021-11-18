4f991187cf8c76ae5a8c4110c1a8220bc1ab78ae
SangNX063 <sang@incognito.org>
2021-11-18 17:07:50 +0700

    update ui pdex-3

---
+++ src/components/wallet/wallet.destop.js
@@ -0,0 +1,57 @@
+import React, {memo} from 'react';
+import {Button, Col, Row} from "antd";
+import logo from '../../assets/incognito-name.png';
+import phone from "../../assets/phone.png";
+import {
+    isMacOs,
+    isIOS,
+} from "react-device-detect";
+
+const Wallet = () => {
+    const onOpenInstall = () => {
+        let url = 'https://play.google.com/store/apps/details?id=com.incognito.wallet';
+        if (isMacOs || isIOS) {
+            url = 'https://apps.apple.com/us/app/incognito-crypto-wallet/id1475631606?ls=1'
+        }
+        window.open(url, '_blank').focus();
+    }
+    return (
+        <div>
+            <Row justify="space-between" gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                <Col>
+                    <img src={logo} className="app-logo"  alt="logo"/>
+                    <p className="wallet-title">
+                        Buy and sell <br/>
+                        crypto privately.
+                    </p>
+                    <div>
+                        <h4 className="wallet-content">
+                            Commission-free trading and total anonymity.<br/>
+                            Your keys, your money, your privacy.
+                        </h4>
+                    </div>
+                    <Button className="btn-start" onClick={onOpenInstall}>
+                        Start trading confidentially
+                    </Button>
+                    <Row className="wrap-category">
+                        <Col>
+                            <p className="category-title">$500 M</p>
+                            <p className="category-content">Trading volume</p>
+                        </Col>
+                        <Col className="center-horizontal">
+                            <p className="category-title">1 M</p>
+                            <p className="category-content">Anonymous trades</p>
+                        </Col>
+                        <Col>
+                            <p className="category-title">1,900</p>
+                            <p className="category-content">Validators</p>
+                        </Col>
+                    </Row>
+                </Col>
+                <img className="img-phone" src={phone} alt="phone"/>
+            </Row>
+        </div>
+    );
+};
+
+export default memo(Wallet);
--- src/containers/desktop/App.js
+++ src/containers/desktop/App.js
@@ -4,17 +4,17 @@ import searchicon from '../../assets/retry.png';
 import axios from 'axios';
 import {Row,Col, Avatar, Input, Button, Layout, Menu, Breadcrumb } from 'antd';
 import Dex from '../../components/dex/index';
-import Wallet from '../../components/wallet/index';
+import Wallet from '../../components/wallet/wallet.destop';
 import Events from '../../components/events/index';
 import Checking from '../../components/checking/index';
 import Faq from '../../components/faq/index';
-import './App.scss'; 
+import './App.scss';
 import WAValidator from '@swyftx/api-crypto-address-validator';
 import {INCO_CHAIN} from '../../services/const';
 import GA from '../../services/GaEvents';
 import { withRouter } from "react-router";
 import { BrowserRouter as Router, Switch,  Route, Link } from "react-router-dom";
-import AboutUs from './AboutUs'; 
+import AboutUs from './AboutUs';
 
 const { Header, Content, Footer } = Layout;
 
@@ -22,69 +22,69 @@ const { Header, Content, Footer } = Layout;
 class App extends React.Component{
 
   constructor(pros){
-    super(pros); 
+    super(pros);
     this.state = {
       address: null,
       network:null,
       loading:false,
       error:false,
-    }; 
-     
-  }  
-    
-  render(){ 
+    };
+
+  }
+
+  render(){
     //console.log(this.props);
     const selected = (this.props.location.pathname =="/" ? '/home' : this.props.location.pathname);
     console.log("selected",selected);
-    return ( 
+    return (
         <Layout className="layout" >
-          <Header className="header-cus">
-            <Row  gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} style={{margin:0}} >  
-            <Col span={12} style={{ textAlign:"left", padding:'0'}} >
-              <Link to={'/'}>
-                  {/* <Avatar size={40} style={{position:'absolute',top:'12px',marginLeft:'0px',backgroundColor:'#000000'}} /> */}
-                  <img src={logo}  style={{width:'158px',position:'absolute',top:'30px',marginLeft:'0px'}} />
-              </Link> 
-              </Col>
-              <Col  span={12} style={{ textAlign:"right", padding:'0'}} > 
-                <Menu className="topMenu" mode="horizontal" selectedKeys={[selected]} style={{marginTop:'20px'}}> 
-                <Menu.Item key="/dex"><Link className="dex" to={'/dex'}>pDEX</Link></Menu.Item>
-                <Menu.Item key="/home"><Link className="dex" to={'/'}>Wallet</Link></Menu.Item>
-                <Menu.Item key="/faq"><Link className="dex" to={'/faq'}>FAQs</Link></Menu.Item>
-                <Menu.Item key="/about-you"> <Link className="about-you" to={'/about-you'}>About you</Link></Menu.Item> 
-                <Menu.Item key="/about-us"> <a className="about-us" href='https://we.incognito.org/'>About us</a>  </Menu.Item>
-                </Menu>
-              </Col> 
-          </Row>
-          </Header> 
-          <Content className="content">    
+          {/*<Header className="header-cus">*/}
+          {/*  <Row  gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} style={{margin:0}} >  */}
+          {/*  <Col span={12} style={{ textAlign:"left", padding:'0'}} >*/}
+          {/*    <Link to={'/'}>*/}
+          {/*        /!* <Avatar size={40} style={{position:'absolute',top:'12px',marginLeft:'0px',backgroundColor:'#000000'}} /> *!/*/}
+          {/*        <img src={logo}  style={{width:'158px',position:'absolute',top:'30px',marginLeft:'0px'}} />*/}
+          {/*    </Link> */}
+          {/*    </Col>*/}
+          {/*    <Col  span={12} style={{ textAlign:"right", padding:'0'}} > */}
+          {/*      <Menu className="topMenu" mode="horizontal" selectedKeys={[selected]} style={{marginTop:'20px'}}> */}
+          {/*      <Menu.Item key="/dex"><Link className="dex" to={'/dex'}>pDEX</Link></Menu.Item>*/}
+          {/*      <Menu.Item key="/home"><Link className="dex" to={'/'}>Wallet</Link></Menu.Item>*/}
+          {/*      <Menu.Item key="/faq"><Link className="dex" to={'/faq'}>FAQs</Link></Menu.Item>*/}
+          {/*      <Menu.Item key="/about-you"> <Link className="about-you" to={'/about-you'}>About you</Link></Menu.Item> */}
+          {/*      <Menu.Item key="/about-us"> <a className="about-us" href='https://we.incognito.org/'>About us</a>  </Menu.Item>*/}
+          {/*      </Menu>*/}
+          {/*    </Col> */}
+          {/*</Row>*/}
+          {/*</Header> */}
+          <Content className="content">
             <Switch>
              <Route path="/" exact  >
-                  <Wallet />   
+                  <Wallet />
                 </Route>
                 <Route path="/dex" exact  >
-                  <Dex />   
+                  <Dex />
                 </Route>
                 <Route path="/quest" exact  >
-                  <Events />   
-                </Route> 
+                  <Events />
+                </Route>
                 <Route path="/faq" exact  >
-                  <Faq />   
-                </Route> 
+                  <Faq />
+                </Route>
                 <Route path="/wallet" exact  >
-                  <Wallet />   
+                  <Wallet />
                 </Route>
                 <Route path="/about-you" exact>
                     <Checking/>
                 </Route>
                     <Route path="/about-us" exact>
                 <AboutUs/>
-                </Route>  
+                </Route>
               </Switch>
           </Content>
           {/* <Footer className="footer-text">Incognito is an <span className="blue">open-source blockchain</span> built by a <span className="blue">community of privacy geeks</span>.</Footer> */}
         </Layout>
-    
+
     );
   }
 }
